ff7dc71121e8177d010a1d184bc7e30529e65eee
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-01-14 20:16:39 +0100

    add: roles & users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,18 @@
+import fr.wadouk.wishlist.Role
+import fr.wadouk.wishlist.User
+import fr.wadouk.wishlist.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        def roleAdmin = new Role(authority: "ROLE_ADMIN").save()
+        def roleUser = new Role(authority: "ROLE_USER").save()
+
+        def userAdmin = new User(username: "admin", password: "admin", enabled: true).save()
+        def userBasic = new User(username: "hello", password: "hello", enabled: true).save()
+
+        UserRole.create(userAdmin,roleAdmin,true)
+        UserRole.create(userBasic,roleUser,true)
     }
     def destroy = {
     }
